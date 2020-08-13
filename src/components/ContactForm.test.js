33739e22d8978e308fd8c8168def313618529f33
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-08-13 17:36:05 -0400

    firstName input test in place

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 
 test("renders ContactForm correctly", () => {
     render(<ContactForm />);
+});
+
+test ("First Name input requires a minimum of two characters", () => {
+    const { getByName } = render(<ContactForm />);
+    const input = getByName(/firstname/i);
+    fireEvent.change(input, { target: {value: "Ed"} });
+    expect(input.value).toBe("Ed");
 });
