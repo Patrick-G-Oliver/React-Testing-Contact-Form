5f2847d30167d4b114859a17cc3e58a078e531c1
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-08-13 21:45:29 -0400

    input acceptance and firstName 2 char min tests complete

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,14 +1,28 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, waitfor, waitFor } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 
 test("renders ContactForm correctly", () => {
     render(<ContactForm />);
 });
 
-test ("First Name input requires a minimum of two characters", () => {
+test ("first Name input accepts a 2-character input ", () => {
     const { getByTestId } = render(<ContactForm />);
     const input = getByTestId(/1stName/i);
+
     fireEvent.change(input, { target: {value: "Ed"} });
     expect(input.value).toBe("Ed");
+});
+
+test ("first name input requires 2 characters minimun", async () => {
+    const { getByTestId, queryByText } = render(<ContactForm />);
+    const firstNameInput = getByTestId(/1stname/i);
+
+    fireEvent.change(firstNameInput, { target: {value: "E"} });
+
+    fireEvent.focusOut(firstNameInput);
+
+    await waitFor( () => {
+        expect(queryByText(/looks/i));
+    })
 });
