aa2d37639494e3e414c5257aa38fa5d7e1d448e0
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-08-13 22:36:35 -0400

    seven test in place in ContactForm.test.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -32,6 +32,7 @@ const ContactForm = () => {
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
+            data-testid="lastName"
           />
           {errors.lastName && (
             <p>Looks like there was an error: {errors.lastName.type}</p>
@@ -42,14 +43,22 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name="email" 
+            ref={register({ required: true })} 
+            data-testid="email"
+          />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
+
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea 
+            name="message" 
+            ref={register({ required: false, maxLength: 10 })} 
+            data-testid="messageField"
+          />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,19 +1,53 @@
 import React from "react";
-import { render, fireEvent, waitfor, waitFor } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 
+// Test 1
 test("renders ContactForm correctly", () => {
     render(<ContactForm />);
 });
 
-test ("first Name input accepts a 2-character input ", () => {
+// Test 2
+test ("firstName input accepts a 2-character input ", () => {
     const { getByTestId } = render(<ContactForm />);
     const input = getByTestId(/1stName/i);
 
     fireEvent.change(input, { target: {value: "Ed"} });
+
     expect(input.value).toBe("Ed");
 });
 
+// Test 3
+test ("email input accepts text input", () => {
+    const { getByTestId } = render(<ContactForm />);
+    const input = getByTestId(/email/i);
+    
+    fireEvent.change(input, { target: {value: "myemail@mail.com"} });
+
+    expect(input.value).toBe("myemail@mail.com");
+});
+
+// Test 4
+test ("lastName input accepts text input", () => {
+    const { getByTestId } = render(<ContactForm />);
+    const input = getByTestId(/lastName/i);
+    
+    fireEvent.change(input, { target: {value: "Wood"} });
+
+    expect(input.value).toBe("Wood");
+});
+
+// Test 5
+test ("message input accepts text input", () => {
+    const { getByTestId } = render(<ContactForm />);
+    const input = getByTestId(/messageField/i);
+    
+    fireEvent.change(input, { target: {value: "Hello, World!"} });
+
+    expect(input.value).toBe("Hello, World!");
+});
+
+// Test 6
 test ("first name input requires 2 characters minimun", async () => {
     const { getByTestId, queryByText } = render(<ContactForm />);
     const firstNameInput = getByTestId(/1stname/i);
@@ -23,6 +57,23 @@ test ("first name input requires 2 characters minimun", async () => {
     fireEvent.focusOut(firstNameInput);
 
     await waitFor( () => {
-        expect(queryByText(/looks/i));
-    })
+        expect(queryByText(/looks like there was an error: /i));
+    });
 });
+
+// Test 7
+test ("messageField allows max 10 chars", async () => {
+    const { getByTestId, queryByText } = render(<ContactForm />);
+    const messageField = getByTestId(/messageField/i);
+
+    fireEvent.change(messageField, { target: {value: "antiestablishmentarianism"} });
+
+    fireEvent.focusOut(messageField);
+
+    await waitFor( () => {
+        expect(queryByText(/looks like there was an error: /i))
+    });
+});
+
+
+
